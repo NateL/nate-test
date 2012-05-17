9542976d72cbf1cbc958955f03eb664d2a9b0a5d
Nathan Lapinski <nathan@mycca.net>
2012-05-17 11:23:43 -0600

    adding mvc test project

---
+++ Mvc3Razor3/Mvc3Razor3/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc3Razor3.Models;
+
+namespace Mvc3Razor3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static Users _usrs = new Users();
+
+        public ActionResult Index()
+        {
+            
+            return View(_usrs._usrList);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Details(string id)
+        {
+            return View(_usrs.GetUser(id));
+        }
+
+        public ViewResult Edit(string id)
+        {
+            return View(_usrs.GetUser(id));
+        }
+
+        [HttpPost]
+        public ViewResult Edit(UserModel um)
+        {
+
+            if (!TryUpdateModel(um))
+            {
+                ViewBag.updateError = "Update Failure";
+                return View(um);
+            }
+
+            // ToDo: add persistent to DB.
+            _usrs.Update(um);
+            return View("Details", um);
+        }
+
+        public ViewResult Create()
+        {
+            return View(new UserModel());
+        }
+
+        [HttpPost]
+        public ViewResult Create(UserModel um)
+        {
+
+            if (!TryUpdateModel(um))
+            {
+                ViewBag.updateError = "Create Failure";
+                return View(um);
+            }
+
+            // ToDo: add persistent to DB.
+            _usrs.Create(um);
+            return View("Details", um);
+        }
+
+        public ViewResult Delete(string id)
+        {
+            return View(_usrs.GetUser(id));
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult Delete(string id, FormCollection collection)
+        {
+            _usrs.Remove(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
